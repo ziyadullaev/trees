0771a0b78a2340c0bf114e1099337ebde3b32a75
ziyodullaev <ogabekziyodullayev3@gmail.com>
2024-12-16 08:11:12 +0500

    added news

---
+++ index.js
@@ -0,0 +1,58 @@
+// 1-masala yigindini hisoblas
+
+// For13: Berilgan yig'indini hisoblash
+// let n = parseInt(prompt("n ni kiriting (n > 0): "));
+// let S = 0;
+// let ishora = 1;
+// for (let i = 1; i <= n; i++) {
+//     S += ishora * (i / 10);
+//     ishora *= -1;
+// }
+// console.log("Yig'indi S =", S);
+
+// 2-chi masala faktaryal
+
+
+// n = parseInt(prompt("son kiriting: "));
+// S = 0;
+// let faktorial = 1;
+// for (let i = 1; i <= n; i++) {
+//     faktorial *= i;
+//     S += faktorial;
+// }
+// console.log("yigindi S =", S);
+
+
+// 3chi masala kvadrati kiritilgan sondan katta kichik eng katta butun
+// n = parseInt(prompt("son kiriting: "));
+// let k = 1;
+// while (k ** 2 <= n) {
+//     k++;
+// }
+// k--;
+// console.log("K son:", k);
+
+
+// 4-masala  kvadrati ndan katta en kichik butun
+
+// n = parseInt(prompt("n ni kiriting (n > 0): "));
+// k = 1;
+// while (k ** 2 <= n) {
+//     k++;
+// }
+// console.log("K qiymati:", k);
+
+
+// 5-masala kiritilgan songacha bolgan toq sonlar
+
+// n = parseInt(prompt("Son kiriting: "));
+// let k = 0;
+// for (let i = 1; i < n; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//         k++;
+//     }
+// }        
+// console.log("toq sonlar soni:", k);
+
+
